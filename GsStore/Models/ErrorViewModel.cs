877375c5bcb3a60089d73a147b647d1f63f9e005
OrtigosaLaura <lauraortigosa378@gmail.com>
2025-02-17 08:01:06 -0300

    fffffffffffffffffffffffffffffffffffff

--- GsStore/Models/ErrorViewModel.cs
+++ GsStore/Models/ErrorViewModel.cs
@@ -2,7 +2,7 @@ namespace GsStore.Models;
 
 public class ErrorViewModel
 {
-    public string? RequestId { get; set; }
+    public string RequestId { get; set; }
 
     public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
 }
